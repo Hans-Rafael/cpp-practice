f43dd5a3dc020e91899c52bdc522b932191aa9a4
hans-rafael <hansvekoni@gmail.com>
2026-03-27 12:24:00 -0300

    ejercicio 3 completado

--- practica_class1/min3.cpp
+++ practica_class1/min3.cpp
@@ -1,1 +1,16 @@
-#include <iostr
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ float hr,sec;
+
+    cout << "¿Cual es la cantidad de horas que deseas ver a cuanto segundos equivale?: ";
+    cin >> hr;
+    sec= hr*3600;
+    cout<<hr<<" horas son "<<sec<<" segundos"<<endl; 
+
+
+    return 0;
+}
