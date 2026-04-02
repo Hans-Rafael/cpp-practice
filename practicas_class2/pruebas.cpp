b7d098cdffad2386a1ea06816660b27408011b23
Your Name <your.email@example.com>
2026-04-02 12:36:49 -0300

    exta comment added to pruebas

--- practicas_class2/pruebas.cpp
+++ practicas_class2/pruebas.cpp
@@ -2,6 +2,19 @@
 #include <iostream>
 using namespace std;
 int main()
+/*
+convertir un estrin a un entero
+stoi(entrada) lo convierte a un numero
+if (stoi(entrada) > 10) */
+/*
+#include <string>
+#include <cctype> // Necesaria para tolower/toupper
+Convertir a MINÚSCULAS
+    for (char &c : texto) {
+        c = tolower(c);
+    }
+*/
+
 {
     int temperature; // La temperatura exterior @author hans-rafael
     // Leer e imprimir por eco la temperatura
@@ -22,12 +35,19 @@ int main()
         cout << "bailar." << endl;
 
     // Usando switch
-    int opcion = 2;
+    /* Reglas a recordar
+    Tipos permitidos: El switch funciona con tipos "integrales" ( int, char, bool, enum).
+     No funciona con: float, double ni string.
+     Char vs String:
+     '+' es un carácter (un solo símbolo).
+     "+" es un string (una cadena, aunque solo tenga un símbolo)
+    */
+    int opcion = 1;
 
     switch (opcion)
     {
     case 1:
-        cout << "Elegiste la opción 1 \n";
+        cout << "Elegiste la opción 1 \n y helado";
         break;
     case 2:
         cout << "Elegiste la opción 2 "; // Esto se ejecutará y recordar usaren line o salto linea ultima linea se puede quedar en el buffer si no
